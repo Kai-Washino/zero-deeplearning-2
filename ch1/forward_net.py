09ed59d874124f68390e8148e0908f5a9abfadff
Kai Washino <79495772+Kai-Washino@users.noreply.github.com>
2024-05-25 15:35:05 +0900

    Add TwoLayerNet

--- ch1/forward_net.py
+++ ch1/forward_net.py
@@ -15,3 +15,35 @@ class Affine:
         W, b = self.params
         out = np.dot(x, W) + b
         return out
+    
+class TwoLayerNet:
+    def __init__(self, input_size, hidden_size, output_size):
+        I, H, O = input_size, hidden_size, output_size
+
+        W1 = np.random.randn(I, H)
+        b1 = np.random.randn(H)
+        W2 = np.random.randn(H, O)
+        b2 = np.random.randn(O)
+
+        self.layers = [
+            Affine(W1, b1),
+            Sigmoid(),
+            Affine(W2, b2)
+        ]
+
+        self.params = []
+        for layer in self.layers:
+            self.params += layer.params
+    
+    def predict(self, X):
+        for layer in self.layers:
+            X = layer.forward(X)
+        return X
+
+
+if __name__ == '__main__':
+    X = np.random.randn(10, 2)
+    print(X.shape)
+    model = TwoLayerNet(2, 4, 3)
+    s = model.predict(X)
+    print(s.shape)
